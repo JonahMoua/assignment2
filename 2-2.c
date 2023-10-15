fa64a7b601540fbf677242a437565e83d17635fc
jmoua123 <nraugnaagmouaa@gmail.com>
2023-10-14 20:34:24 -0700

    2-2 solution

--- 2-2.c
+++ 2-2.c
@@ -1,11 +1,22 @@
-//Used chatGPT, Stackoverflow and github to do this assignment
 #include <stdio.h>
 
 unsigned int extract(unsigned int x, int i) {
-    // Shift the byte i want to the right towards the least significant position 
-    // FF is a placeholder which gents replaced 
-    return (x >> (i * 8)) & 0xFF;
+    // we calculate the shift amount. i << 3 is equivalent to i * 8
+    unsigned int shift = i << 3;
+    // This line extracts the desired byte from x. 
+    // It first shifts x right by the calculated shift bits (which is i * 8) to position the byte of interest as the least significant byte. 
+    // Then, it applies a bitmask 0xFF (which is 00000000 00000000 00000000 11111111 in binary) 
+    // to extract only the least significant 8 bits, effectively isolating the desired byte.
+    unsigned int result = (x >> shift) & 0xFF;
+
+    // Calculate the sign bit and sign-extend
+    // Here, we determine the sign bit of the extracted byte and create a sign extension mask. 
+    // The expression (result & 0x80) checks if the most significant bit (the sign bit) of result is set. If it is, it returns 0xFFFFFF00, which is a sign extension for a negative number. 
+    // If the sign bit is not set (indicating a positive or zero value), it returns 0x00, which signifies no sign extension
+    unsigned int signBit = (result & 0x80) ? 0xFFFFFF00 : 0x00;
+    return result | signBit;
 }
+
 
 int main() {
     unsigned int x;
@@ -15,18 +26,13 @@ int main() {
     x = 0x12345678;
     i = 0;
     unsigned int result = extract(x, i);
-    if (result != 0) {
-        printf("extract(0x%08X, %d): 0x%08X\n", x, i, result);
-    }
+    printf("extract(0x%08X, %d): 0x%08X\n", x, i, result);
 
-    // Test the extract function for value x = 0xABCDEF00 and i value of 0 and 2.
+    // Test the extract function for value x = 0xABCDEF00 and i value of 2.
     x = 0xABCDEF00;
-    for (i = 0; i <= 2; i += 2) {
-        unsigned int result = extract(x, i);
-        if (result != 0) {
-            printf("extract(0x%08X, %d): 0x%08X\n", x, i, result);
-        }
-    }
+    i = 2; // Set i to the desired value
+    unsigned int result2 = extract(x, i);
+    printf("extract(0x%08X, %d): 0x%08X", x, i, result2);
 
     return 0;
 }
