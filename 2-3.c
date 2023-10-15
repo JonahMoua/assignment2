b12d941100cdfb93f79a8e4e01cc7cd806530709
jmoua123 <nraugnaagmouaa@gmail.com>
2023-10-14 22:50:17 -0700

    2-2 solution please look over

--- 2-3.c
+++ 2-3.c
@@ -8,81 +8,81 @@ int ge(float x, float y) {
     unsigned int sy = uy >> 31; // extract sign bit of uy   
     ux &= 0x7FFFFFFF; // drop sign bit of ux
     uy &= 0x7FFFFFFF; // drop sign bit of uy
-    // TODO: return using sx, sy, ux, uy
 
-    // Calculate the sign difference
-    unsigned int signDiff = sx ^ sy;
+    // the first condition is to check if both x and y are 0 and return 1 if true
+    // the second condition is to check if they are the same sign positive/negative and if x is greater than or equal to uy
+    // the last condition is a simple check to see if x is positive and y is negative to make it declared 1 immediately
+    return (ux == 0 && uy == 0) || (sx == sy && ux >= uy) || (sx == 0 && sy == 1);
 
-    // Calculate the result
-    return ((!(signDiff | ux - uy)) | (sx & !(sy))) & 1;
 }
+
 
 int main() {
     float a, b;
 
     a = 0.0f;
     b = 0.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -0.0f;
     b = 0.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 0.0f;
     b = -0.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -0.0f;
     b = -0.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 1.0f;
     b = 1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -1.0f;
     b = 1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 1.0f;
     b = -1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -1.0f;
     b = -1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -1.0f;
     b = 0.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 0.0f;
     b = -1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 1.0f;
     b = 0.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 0.0f;
     b = 1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 1.0f;
     b = 2.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = 2.0f;
     b = 1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -1.0f;
     b = -2.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     a = -2.0f;
     b = -1.0f;
-    printf("ge(%f, %f): %d\n", a, b, ge(a, b));
+    printf("ge(%.1f, %.1f): %d\n", a, b, ge(a, b));
 
     return 0;
 }
